fb122029ed800f676010eef2000573d70e9eb32f
Daniel Ye <danielyeqt@gmail.com>
2022-12-13 13:34:02 -0500

    Add slow movement & photoresistor testing

--- movement_control_v1/movement_control_v1.ino
+++ movement_control_v1/movement_control_v1.ino
@@ -1,16 +1,3 @@
-// minimum unit needed on a pin to get the motor to run
-int motorPower = 160;
-// the maximum value of customizable power you can 
-int motorMax = 255 - motorPower;
-
-// clips the customizable motor power within a range, and notifies the user if it is out of the allowed range
-void clipPower(int& power){
-  if(power < 0 || power > motorMax){
-    Serial.println("Invalid Power");
-  }
-  power = max(0, min(motorMax, power));
-}
-
 // runs a motor
 struct Motor {
   // control (power) pin, out1 point and out2 pin (for directions)
@@ -25,11 +12,8 @@ struct Motor {
 
   // moves the motor forward with [power] power
   void forward(int power){
-    // notify user if the power is too large and clip it
-    clipPower(power);
-
     // write power needed to control
-    analogWrite(ctl, motorPower + power);
+    analogWrite(ctl, power);
 
     // set the direction
     digitalWrite(out1, HIGH);
@@ -38,11 +22,8 @@ struct Motor {
 
   // move the motor backwrad with [power] power
   void backward(int power){
-    // notify user if power is out of range and clip it
-    clipPower(power);
-
     // write power needed to ctl pin
-    analogWrite(ctl, motorPower + power);
+    analogWrite(ctl, power);
 
     // set the direction
     digitalWrite(out1, LOW);
@@ -77,32 +58,83 @@ struct Drivetrain{
     right.stop();
   }
 
-  // moves the chassis forward
-  void forward(int power){
-    clipPower(power);
-    left.forward(power);
-    right.forward(power);
+  // moves the chassis forward, should be called once at the start of the movement
+  void forward(int mode){
+    if(mode == 0){
+      left.forward(190);
+      right.forward(145);
+    } else if(mode == -1){
+      // slow - adds voltage for momentum, then goes at a slow pace
+      left.forward(160);
+      right.forward(170);
+      delay(200);
+      left.forward(90);
+      right.forward(95);
+    } else {
+      Serial.println("invalid forward mode: " + mode);
+    } 
   }
 
-  // moves the chassis backward
-  void backward(int power){
-    clipPower(power);
-    left.backward(power);
-    right.backward(power);
+  // moves the chassis backward, should be called once at the start of the movement
+  void backward(int mode){
+    if(mode == 0){
+      left.backward(190);
+      right.backward(145);
+    } else if(mode == -1){
+      // slow - adds voltage for momentum, then goes at a slow pace
+      left.backward(160);
+      right.backward(170);
+      delay(300);
+      left.backward(90);
+      right.backward(95);
+    }else {
+      Serial.println("invalid backward mode: " + mode);
+    }
   }
 
   // rotates the chassis clockwise
-  void clockwise(int power){
-    clipPower(power);
-    left.forward(power);
-    right.backward(power);
+  void clockwise(int mode){
+    if(mode == 0){
+      left.forward(190);
+      right.backward(145);
+    } else {
+      Serial.println("invalid CW mode: " + mode);
+    }
   }
 
   // rotates the chassis counterclockwise
-  void counterClockwise(int power){
-    clipPower(power);
-    left.backward(power);
-    right.forward(power);
+  void counterClockwise(int mode){
+    if(mode == 0){
+      left.backward(190);
+      right.forward(145);
+    }else {
+      Serial.println("invalid CCW mode: " + mode);
+    }
+  }
+
+  // turn [angle] degrees clockwise, make [angle] negative for CCW turns, then stop
+  void turn(int angle){
+    if(angle == 90){
+      clockwise(0);
+      delay(450);
+      stop();
+    }
+  }
+};
+struct PhotoResistor{
+  int pin;
+  const int THRESHOLD = 45;
+  PhotoResistor(int pin){
+    this->pin = pin;
+  }
+  void setup(){
+    pinMode(pin, INPUT);
+  }
+  int read(){
+    return analogRead(pin);
+  }
+  bool triggered(){
+    return read() >= THRESHOLD;
   }
 };
 
@@ -110,19 +142,23 @@ Motor left = {3, 2, 4};
 Motor right = {9, 7, 8};
 // the chassis' drivetrain
 Drivetrain drivetrain(left, right);
+PhotoResistor center(A0);
 
 void setup(){
   drivetrain.setup();
+  center.setup();
   Serial.begin(9600);
+  drivetrain.forward(-1);
 }
 
+bool stopped = false;
 void loop() {
-  drivetrain.forward(50);
-  delay(1000);
-  drivetrain.backward(50);
-  delay(1000);
-  drivetrain.clockwise(50);
-  delay(1000);
-  drivetrain.counterClockwise(50);
-  delay(1000);
+  Serial.println(center.read());
+  if(!center.triggered() && !stopped){
+    stopped = true;
+    delay(10);
+    drivetrain.backward(0);
+    delay(10);
+    drivetrain.stop();
+  }
 }
