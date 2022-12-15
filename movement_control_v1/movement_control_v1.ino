36263c9a7818295d4ea4df2812ce9caf00137d6f
Daniel Ye <danielyeqt@gmail.com>
2022-12-15 13:01:47 -0500

    Change to constants

--- movement_control_v1/movement_control_v1.ino
+++ movement_control_v1/movement_control_v1.ino
@@ -1,3 +1,6 @@
+const int IMPULSE[2] = {255, 255}, SUSTAIN[2] = {140, 140}, STRAIGHT_DELAY=300;
+const int DELAY_90 = 500;
+
 // runs a motor
 struct Motor {
   // control (power) pin, out1 point and out2 pin (for directions)
@@ -61,15 +64,15 @@ struct Drivetrain{
   // moves the chassis forward, should be called once at the start of the movement
   void forward(int mode){
     if(mode == 0){
-      left.forward(190);
-      right.forward(145);
+      left.forward(IMPULSE[0]);
+      right.forward(IMPULSE[1]);
     } else if(mode == -1){
       // slow - adds voltage for momentum, then goes at a slow pace
-      left.forward(160);
-      right.forward(170);
-      delay(200);
-      left.forward(90);
-      right.forward(95);
+      left.forward(IMPULSE[0]);
+      right.forward(IMPULSE[1]);
+      delay(STRAIGHT_DELAY);
+      left.forward(SUSTAIN[0]);
+      right.forward(SUSTAIN[1]);
     } else {
       Serial.println("invalid forward mode: " + mode);
     } 
@@ -78,15 +81,15 @@ struct Drivetrain{
   // moves the chassis backward, should be called once at the start of the movement
   void backward(int mode){
     if(mode == 0){
-      left.backward(190);
-      right.backward(145);
+      left.backward(IMPULSE[0]);
+      right.backward(IMPULSE[1]);
     } else if(mode == -1){
       // slow - adds voltage for momentum, then goes at a slow pace
-      left.backward(160);
-      right.backward(170);
-      delay(300);
-      left.backward(90);
-      right.backward(95);
+      left.backward(IMPULSE[0]);
+      right.backward(IMPULSE[1]);
+      delay(STRAIGHT_DELAY);
+      left.backward(SUSTAIN[0]);
+      right.backward(SUSTAIN[1]);
     }else {
       Serial.println("invalid backward mode: " + mode);
     }
@@ -95,8 +98,8 @@ struct Drivetrain{
   // rotates the chassis clockwise
   void clockwise(int mode){
     if(mode == 0){
-      left.forward(190);
-      right.backward(145);
+      left.forward(IMPULSE[0]);
+      right.backward(IMPULSE[1]);
     } else {
       Serial.println("invalid CW mode: " + mode);
     }
@@ -105,8 +108,8 @@ struct Drivetrain{
   // rotates the chassis counterclockwise
   void counterClockwise(int mode){
     if(mode == 0){
-      left.backward(190);
-      right.forward(145);
+      left.backward(IMPULSE[0]);
+      right.forward(IMPULSE[1]);
     }else {
       Serial.println("invalid CCW mode: " + mode);
     }
@@ -116,7 +119,7 @@ struct Drivetrain{
   void turn(int angle){
     if(angle == 90){
       clockwise(0);
-      delay(450);
+      delay(DELAY_90);
       stop();
     }
   }
@@ -156,9 +159,14 @@ void loop() {
   Serial.println(center.read());
   if(!center.triggered() && !stopped){
     stopped = true;
-    delay(10);
+    delay(50);
     drivetrain.backward(0);
     delay(10);
     drivetrain.stop();
+    delay(500);
+    drivetrain.turn(90);
+    delay(1000);
+    drivetrain.forward(-1);
+    stopped = false;
   }
 }
