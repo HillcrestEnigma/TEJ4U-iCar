d0cca166788dbf733f89fbd0027f347755f3f4e8
Daniel Ye <danielyeqt@gmail.com>
2022-12-22 14:55:53 -0500

    Abstraction & Plan

--- movement_control_v1/movement_control_v1.ino
+++ movement_control_v1/movement_control_v1.ino
@@ -1,7 +1,9 @@
-const int IMPULSE[2] = {80, 80}, SUSTAIN[2] = {40, 40}, STRAIGHT_DELAY=300;
-const int RIGHT_SUSTAIN[2] = {50, 40}, LEFT_SUSTAIN[2] = {40, 50};
+const int IMPULSE_POWER[2] = {80, 80}, SUSTAIN_POWER[2] = {40, 40}, TURN_POWER[2] = {60, 60}, STRAIGHT_DELAY=300;
+const int RIGHT_SUSTAIN_POWER[2] = {50, 40}, LEFT_SUSTAIN_POWER[2] = {40, 50};
 const int DELAY_90 = 500;
-const int WAIT=-1, FORWARD_STATE = 0, TILT_RIGHT = 1, TILT_LEFT = 2, TURN_LEFT = 3, TURN_RIGHT = 4;
+const int WAIT=-1, FORWARD_STATE = 0, TILT_RIGHT = 1, TILT_LEFT = 2, LEFT_CORNER = 3, RIGHT_CORNER = 4, TEST_OFF = 5;
+
+const int OFF_BUFFER = 100;
 
 // runs a motor
 struct Motor {
@@ -42,6 +44,8 @@ struct Motor {
   // stops the motor
   void stop(){
     digitalWrite(ctl, LOW);
+    digitalWrite(out1, LOW);
+    digitalWrite(out2, LOW);
   }
 };
 
@@ -51,13 +55,7 @@ struct Drivetrain {
   Motor left, right;
 
   // class constructor
-  Drivetrain(Motor left, Motor right){
-    this->left = left;
-    this->right = right;
-
-    left.setup();
-    right.setup();
-  }
+  Drivetrain(Motor& left, Motor& right): left(left), right(right) {}
 
   // stops the drivetrain
   void stop(){
@@ -72,25 +70,25 @@ struct Drivetrain {
       mode 2: moves to the right a bit
       mode 3: moves to the left a bit
   */
-  static const int FORWARD = 1, LEFT = 3, RIGHT = 2;
+  static const int IMPULSE = -1, FORWARD_FAST = 0, FORWARD = 1, LEFT = 2, RIGHT = 3;
   void forward(int mode){
-    if(mode == -1){
-      left.forward(IMPULSE[0]);
-      right.forward(IMPULSE[1]);
+    if (mode == IMPULSE) {
+      left.forward(IMPULSE_POWER[0]);
+      right.forward(IMPULSE_POWER[1]);
       delay(STRAIGHT_DELAY);
-    } else if(mode == 0){
-      left.forward(IMPULSE[0]);
-      right.forward(IMPULSE[1]);
+    } else if(mode == FORWARD_FAST){
+      left.forward(IMPULSE_POWER[0]);
+      right.forward(IMPULSE_POWER[1]);
     } else if(mode == FORWARD){
       // slow - adds voltage for momentum, then goes at a slow pace
-      left.forward(SUSTAIN[0]);
-      right.forward(SUSTAIN[1]);
+      left.forward(SUSTAIN_POWER[0]);
+      right.forward(SUSTAIN_POWER[1]);
     } else if(mode == RIGHT){
-      left.forward(RIGHT_SUSTAIN[0]);
-      right.forward(RIGHT_SUSTAIN[1]);
+      left.forward(RIGHT_SUSTAIN_POWER[0]);
+      right.forward(RIGHT_SUSTAIN_POWER[1]);
     } else if(mode == LEFT){
-      left.forward(LEFT_SUSTAIN[0]);
-      right.forward(LEFT_SUSTAIN[1]);
+      left.forward(LEFT_SUSTAIN_POWER[0]);
+      right.forward(LEFT_SUSTAIN_POWER[1]);
     } else {
       Serial.println("invalid forward mode: " + mode);
     } 
@@ -98,17 +96,17 @@ struct Drivetrain {
 
   // moves the chassis backward, should be called once at the start of the movement
   void backward(int mode){
-    if(mode == 0){
-      left.backward(IMPULSE[0]);
-      right.backward(IMPULSE[1]);
-    } else if(mode == -1){
+    if (mode == 0) {
+      left.backward(IMPULSE_POWER[0]);
+      right.backward(IMPULSE_POWER[1]);
+    } else if (mode == -1) {
       // slow - adds voltage for momentum, then goes at a slow pace
-      left.backward(IMPULSE[0]);
-      right.backward(IMPULSE[1]);
+      left.backward(IMPULSE_POWER[0]);
+      right.backward(IMPULSE_POWER[1]);
       delay(STRAIGHT_DELAY);
-      left.backward(SUSTAIN[0]);
-      right.backward(SUSTAIN[1]);
-    }else {
+      left.backward(SUSTAIN_POWER[0]);
+      right.backward(SUSTAIN_POWER[1]);
+    } else {
       Serial.println("invalid backward mode: " + mode);
     }
   }
@@ -116,8 +114,8 @@ struct Drivetrain {
   // rotates the chassis clockwise
   void clockwise(int mode){
     if(mode == 0){
-      left.forward(IMPULSE[0]);
-      right.backward(IMPULSE[1]);
+      left.forward(TURN_POWER[0]);
+      right.backward(TURN_POWER[1]);
     } else {
       Serial.println("invalid CW mode: " + mode);
     }
@@ -126,8 +124,8 @@ struct Drivetrain {
   // rotates the chassis counterclockwise
   void counterClockwise(int mode){
     if(mode == 0){
-      left.backward(IMPULSE[0]);
-      right.forward(IMPULSE[1]);
+      left.backward(TURN_POWER[0]);
+      right.forward(TURN_POWER[1]);
     } else {
       Serial.println("invalid CCW mode: " + mode);
     }
@@ -145,11 +143,11 @@ struct PhotoResistor{
   PhotoResistor(int pin, int threshold){
     this->pin = pin;
     this->threshold = threshold;
-    lasRecord = 0;
-  }
-  void setup(){
+    this->lasRecord = 0;
+
     pinMode(pin, INPUT);
   }
+
   int read(){
     if(millis() - lasRecord > RECORD_DELAY){
       for(int i = 0; i < NUM_RECORD - 1; ++i) prev[i] = prev[i + 1];
@@ -168,21 +166,26 @@ struct PhotoResistor{
 struct SensorArray {
   PhotoResistor left, centre, right;
 
-  SensorArray(PhotoResistor left, PhotoResistor centre, PhotoResistor right){
-    this->left = left;
-    this->centre = centre;
-    this->right = right;
-  }
-}
-
-struct Car {
+  SensorArray(PhotoResistor& left, PhotoResistor& centre, PhotoResistor& right): left(left), centre(centre), right(right) {}
+  
+  // int& read(){
+  //   return {left.read(), centre.read(), right.read()};
+  // }
 };
 
-Motor left = {3, 2, 4};
-Motor right = {9, 7, 8};
+struct Car {
+  Drivetrain drivetrain;
+  SensorArray sensors;
+
+  Car(Drivetrain& drivetrain, SensorArray& sensors): drivetrain(drivetrain), sensors(sensors) {}
+};
+
+Motor left(3, 2, 4);
+Motor right(9, 7, 8);
 // the chassis' drivetrain
 Drivetrain drivetrain(left, right);
 PhotoResistor leftRes(A2, 200), centerRes(A0, 60), rightRes(A1, 60);
+SensorArray sensors(leftRes, centerRes, rightRes);
 
 void printLights(){
   Serial.println("LEFT CENTER RIGHT");
@@ -194,30 +197,62 @@ void printLights(){
 }
 
 void setup(){
-  drivetrain.setup();
-  leftRes.setup();
-  rightRes.setup();
   Serial.begin(9600);
 }
 
 int state = -1;
-const int WAIT=-1, FORWARD_STATE = 0, TILT_RIGHT = 1, TILT_LEFT = 2, TURN_LEFT = 3, TURN_RIGHT = 4;
+
+// the time at which car went off line
+int offTime = 0;
 void loop() {
   printLights();
   if (state == WAIT) {
+    // waiting to be in a good starting position
     if (millis() > 200 && leftRes.triggered() && rightRes.triggered() && !centerRes.triggered()) {
-      drivetrain.forward(-1);
+      drivetrain.forward(Drivetrain::IMPULSE);
       state = FORWARD_STATE;
     }
   } else if(state == FORWARD_STATE){
+    // moving forwards
     drivetrain.forward(Drivetrain::FORWARD);
+
+    // if the left side on line, tilt left
     if(!leftRes.triggered()) state = TILT_LEFT;
-    if(!rightRes.triggered()) state = TILT_RIGHT;
+    if(!rightRes.triggered()) {
+      // if right side on line
+      if(!centerRes.triggered()) state = RIGHT_CORNER;
+      else state = TILT_RIGHT;
+    }
+
+    // once we're off
+    if(leftRes.triggered() && rightRes.triggered() && centerRes.triggered()){
+      state = TEST_OFF;
+      offTime = millis();
+    }
   } else if(state == TILT_RIGHT){
+    // tilt right
     drivetrain.forward(Drivetrain::RIGHT);
+
+    // if center on line, move forward
     if(!centerRes.triggered()) state = FORWARD_STATE;
   } else if(state == TILT_LEFT){
+    // tilt left
     drivetrain.forward(Drivetrain::LEFT);
+
+    // if center on line, move forward
     if(!centerRes.triggered()) state = FORWARD_STATE;
+  } else if(state == RIGHT_CORNER){
+    // turn right
+    Serial.println("Turning right");
+    drivetrain.clockwise(0);
+
+    // if right side is off the line, then continue moving forward
+    if(rightRes.triggered()) state = FORWARD_STATE;
+  } else if(state == LEFT_CORNER){
+    drivetrain.counterClockwise(0);
+    if(centerRes.triggered()) state = FORWARD_STATE;
+  } else if(state == TEST_OFF){
+    drivetrain.forward(Drivetrain::FORWARD);
+    if(millis() - offTime > OFF_BUFFER) state = LEFT_CORNER;
   }
 }
